e14e3de6111aa18686921dc21eba9edd407b8ce4
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-10-23 18:51:26 +0400

    Working with arguments in script

--- change.py
+++ change.py
@@ -2,10 +2,13 @@ import xml.etree.ElementTree as ET
 import sys
 
 file = sys.argv[1]
+key = sys.argv[2]
+attribute = sys.argv[3]
+value = sys.argv[4]
 xmlFile = open(file, 'r')
 firstLine = xmlFile.readline()
 tree = ET.parse(file)
-tree.find('/post/isFixedPitch').set('value', '1')
+tree.find(key).set(attribute, value)
 tree.write(file)
 
 xmlFile = open(file)
